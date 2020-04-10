fd6bfa173f51947d62af22ee79dba3e0bf5ecdec
Wawesh2020 <ghenry429@gmail.com>
2020-04-10 14:54:10 -0400

    perfect-lineup code all but one tests run effectively

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function validateLineup(lineup)
-{
+
+function validateLineup(lineup) {
   // returns false when the lineup includes too many players from a single team
 
   const getPlayers = lineup
@@ -9,29 +9,34 @@ function validateLineup(lineup)
 
   for (let i = 0; i < getPlayers.length; i++) {
     if (count >= 2) return false
+    if (getPlayers[i] === getPlayers[i + 1]) count++
   }
-  // if (getPlayers[i] === getPlayers[i + 1]) count++
-
 
   const morePlayers = lineup
     .map(lineup => lineup.gameId)
     .sort((a, b) => a - b)
 
+  count = 0
   for (let i = 0; i < morePlayers.length; i++) {
     if (count >= 3) return false
 
-    // if (morePlayers[i] === morePlayers[i + 1]) count++ 
+    if (morePlayers[i] === morePlayers[i + 1]) count++
   }
 
 
   const fewPlayers = lineup
     .map(lineup => lineup.position)
-    // .sort((a, b) => a - b) 
+    .sort()
+
+  // console.log(fewPlayers)
+  count = 0
 
   for (let i = 0; i < fewPlayers.length; i++) {
-    if (lineup.position != 'OF') return false
+    if (fewPlayers[i] === fewPlayers[i + 1]) count++
   }
+  if (count < 2 || count > 2) return false
 
+  // 'P', 'C', '1B', '2B', '3B', 'SS'
 
   const getSalary = lineup.map(lineup => lineup.salary)
   const totalSalary = getSalary.reduce((acc, salary) => acc + salary, 0)
